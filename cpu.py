737e7c3c05d2b2fc3e1797ba8cafc3caed4310c3
Nate <lao.nathan@yahoo.com>
2020-06-14 16:31:43 -0400

    specify number of cores

--- cpu.py
+++ cpu.py
@@ -10,9 +10,13 @@ import signal
 
 INITIAL_TEMP_TIME = 1 #minute
 SAMPLE_RATE=5 #seconds
-SAMPLE_TIME=60 #minutes
+SAMPLE_TIME=10 #minutes
+CORES_MAX=4
 
-filename = "CPU_" + str(datetime.datetime.now()).replace(" ","_")
+filename = "CPU_" + str(datetime.datetime.now())
+filename = filename.replace(" ","_")
+filename = filename.replace(":","_")
+filename = filename.replace(".","_")
 
 # Create log file
 logname = filename + ".log"
@@ -21,7 +25,6 @@ logfile = open(logname,"w+")
 def log(msg):
     print(msg)
     logfile.write("%s\n" % msg)
-
 
 def getClockrate():
     value = subprocess.check_output(["vcgencmd measure_clock arm"],shell=True)
@@ -43,6 +46,22 @@ def getMetrics():
     temp = getTemperature()
     return "%s,%s,%s" % (time,clock,temp)
 
+def stressTest(cpu_cores):
+    if (cpu_cores > 0):
+        pid = os.spawnlp(os.P_NOWAIT, "stress", "stress", "--cpu",str(cpu_cores))
+        log("running stress with base pid=%s, cpu_cores=%d" % (str(pid),cpu_cores))
+    else:
+        log("running baseline measurements")
+    second = 0
+    while (second < SAMPLE_TIME * 60):
+        metrics = getMetrics()
+        log("%s,%d    (%d/%d)" % (metrics,cpu_cores,second/SAMPLE_RATE,SAMPLE_TIME*60/SAMPLE_RATE))
+        data.write("%s,%d\n" % (metrics,cpu_cores))
+        second += SAMPLE_RATE
+        time.sleep(SAMPLE_RATE)
+
+    os.system("killall stress") # the pid of the children threads differ, so use this instead
+
 log("initializing CPU stress test")
 log("TIME=%d, RATE=%d" % (SAMPLE_TIME,SAMPLE_RATE))
 
@@ -50,31 +69,14 @@ log("TIME=%d, RATE=%d" % (SAMPLE_TIME,SAMPLE_RATE))
 results = filename + ".csv"
 data = open(results,"w+")
 log("benchmark results stored in %s" % results)
-data.write("time,clockrate,temperature\n") # column headers
-
-# Get initial temps
-log("measuring initial temperatures")
-seconds=0
-while (seconds < INITIAL_TEMP_TIME * 60):
-    metrics = getMetrics()
-    log(metrics)
-    data.write("%s\n" % metrics)
-    seconds += SAMPLE_RATE
-    time.sleep(SAMPLE_RATE)
+data.write("time,clockrate,temperature,cores\n") # column headers
 
 # Start the stress test
 log("starting stress test")
-pid = os.spawnlp(os.P_NOWAIT, "stress", "stress", "--cpu","4")
-log("running stress with base pid=%s" % str(pid))
-seconds = 0
-while (seconds < SAMPLE_TIME * 60):
-    metrics = getMetrics()
-    log("%s    -> %d:%d (%d:00)" % (metrics,seconds/60,seconds%60,SAMPLE_TIME))
-    data.write("%s\n" % metrics)
-    seconds += SAMPLE_RATE
-    time.sleep(SAMPLE_RATE)
-
-os.system("killall stress") # the pid of the children threads differ, so use this instead
+cores=0
+while (cores <= CORES_MAX):
+    stressTest(cores)
+    cores += 1
 
 log("CPU stress test complete")
 
